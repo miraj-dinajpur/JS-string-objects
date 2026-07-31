87381c5c7db560acac7953ec6c736bd4173ce591
miraj-dinajpur <miraj.dinajpur@gmail.com>
2026-07-31 22:42:59 +0600

    include.js added

---
+++ includes.js
@@ -0,0 +1,10 @@
+const email = 'mama@mami.com'
+if(email.includes('.Com')){
+    console.log('has dot com')
+}
+
+const fileName = 'My Final final final khodar kosom final.png'
+const twitterHandle = '@elonMask'
+
+console.log(fileName.endsWith('.pdf'))
+console.log(twitterHandle.startsWith('@'))
