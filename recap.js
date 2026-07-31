7df6a9e8dabc06868512db3293a20b510f00e243
miraj-dinajpur <miraj.dinajpur@gmail.com>
2026-07-31 22:40:59 +0600

    recap.js added

---
+++ recap.js
@@ -0,0 +1,9 @@
+const goals = 4;
+const player = 'Mess e';
+const isGOlden = true;
+
+if(player === 'mbappe'){
+    console.log('ki khawaise tor bap e')
+}
+
+const matches  = ['vs algeria', 'vs belgium']
