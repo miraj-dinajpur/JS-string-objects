6a3e2e3c2b00e7eb21a8e0445f6fb0937cb41d14
miraj-dinajpur <miraj.dinajpur@gmail.com>
2026-07-31 22:43:47 +0600

    dot.js added

---
+++ dot.js
@@ -0,0 +1,12 @@
+const person = {
+    name: 'tobarak Ali',
+    profession: 'student',
+    age: 21,
+    isSingle: true
+}
+
+const age = person.age;
+console.log(person)
+person.age = 29;
+person.isSingle = false;
+console.log(person)
