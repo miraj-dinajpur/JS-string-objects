1f1ddf72186d8147a054198a617aa59ad4cae9e1
miraj-dinajpur <miraj.dinajpur@gmail.com>
2026-07-31 22:44:39 +0600

    comparison.js added

---
+++ comparison.js
@@ -0,0 +1,25 @@
+const subject = 'chemistry';
+const book = 'CHeMisTry';
+
+// console.log(book.toLowerCase())
+// if(subject.toLowerCase() === book.toLowerCase()){
+//     console.log('Poira poira fatai felbo');
+// }
+// else {
+//     console.log('Aibar ki auto pass ase!!!!')
+// }
+
+const emailUserProvided = '  goni@MIA.com  '
+const emailSavedInDatabase= 'goni@mia.com'
+console.log('####:',emailUserProvided.trimStart().trimEnd().toUpperCase(), '###')
+
+if(emailUserProvided === emailSavedInDatabase){
+    console.log('welcome to the website')
+}
+else{
+    // console.log('DGM--> Door e giye ')
+}
+
+// if(emailUserProvided.toLowerCase() === emailSavedInDatabase.toLowerCase()){
+
+// }
