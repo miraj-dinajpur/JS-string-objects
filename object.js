ba1e585777acc85a46a48721c02597cfb65e4c32
miraj-dinajpur <miraj.dinajpur@gmail.com>
2026-07-31 22:41:35 +0600

    object.js

---
+++ object.js
@@ -0,0 +1,26 @@
+// const cap = ['khakhi', 'CR7', 250, 200]
+
+/**
+ * name : sunglass
+ * price: 120
+ * color: white
+ * 
+ * 
+ * 
+*/
+
+const sunglass = {
+    color: 'white',
+    price: 120,
+    brand: 'Ray Ban'
+}
+
+const phone = {
+    brand: 'Apple',
+    version: 17,
+    isThreeCamera: false,
+    secondHand: false,
+    appsInstalled: ['fb', 'chatgpt', 'neptune']
+}
+
+console.log(phone)
