436f40e7c8f8d7f545c14eb71cacca8ab28167cd
miraj-dinajpur <miraj.dinajpur@gmail.com>
2026-07-31 22:34:03 +0600

    split.js added

---
+++ split.js
@@ -0,0 +1,5 @@
+const companiesStr = 'Tesla SpaceX Paypal NeuraLink SolarCity Twitter';
+const companies = companiesStr.split(' ');
+console.log(companies)
+const againStr = companies.join(' ')
+console.log(againStr)
