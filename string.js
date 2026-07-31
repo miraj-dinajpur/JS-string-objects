1f7d4e3b3c754daeced358e0d3794b017d4b0adc
miraj-dinajpur <miraj.dinajpur@gmail.com>
2026-07-31 22:32:57 +0600

    String.js added

---
+++ string.js
@@ -0,0 +1,16 @@
+const cup = 'World        Cup';
+const tea = "Tea cup";
+const location = 'Dhaka';
+// const vehicle = new String('car');
+
+
+const numbers = [4, 54, 6, 78, 11];
+// console.log(numbers.length);
+// console.log(cup.length);
+
+// console.log(numbers[0])
+// console.log(location[0])
+numbers[0] = 99;
+console.log(numbers);
+location[0] = 'F';
+console.log(location);
